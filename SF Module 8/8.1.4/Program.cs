94a03f6a5424df091d338939f7f00104a872dffe
Dmitry <ryabchikk@rambler.ru>
2022-10-20 18:31:18 +0300

    8.1.4

---
+++ SF Module 8/8.1.4/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class DiskInformation
+
+{
+
+   private string name;
+   private long maxVolume;
+   private long freeSpace;
+
+    public string Name()
+    {
+        return name;
+    }
+    public long MaxVolume()
+    {
+        return maxVolume;
+    }
+
+    public long FreeSpace()
+    {
+        return freeSpace;
+    }
+
+    public DiskInformation(string name, long maxVolume, long freeSpace)
+    {
+        this.name = name;
+        this.maxVolume = maxVolume;
+        this.freeSpace = freeSpace;
+    }   
+}
