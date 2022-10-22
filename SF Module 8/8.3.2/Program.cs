5ecd80f710cd994adc25602a4777354e779e8a39
Dmitry <ryabchikk@rambler.ru>
2022-10-22 21:36:42 +0300

    8.4.1

---
+++ SF Module 8/8.3.2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        string strPath = @"C:\Users\Dima\Google Диск\Repos\SF Module 8\SF Module 8\8.3.2\Program.cs";
+        FileWriter.SourceWriterOnConsoleWithDate(strPath);
+
+
+    }
+}
+
+class FileWriter
+{
+    public static void SourceWriterOnConsole(string sourcePath)
+    {
+        try
+        {
+            // Откроем файл и прочитаем его содержимое
+            using (StreamReader sr = File.OpenText(sourcePath))
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+        catch { }
+    }
+
+
+    public static void SourceWriterOnConsoleWithDate(string sourcePath)
+    {
+        try
+        {
+            DateTime dtNow = DateTime.Now;
+            string strTmp = dtNow.ToString();
+
+            var fileInfo = new FileInfo(sourcePath);
+            using (StreamWriter sw = fileInfo.AppendText())
+            {
+                sw.WriteLine("//Время запуска: " + strTmp);
+
+            }
+
+            // Откроем файл и прочитаем его содержимое
+            using (StreamReader sr = File.OpenText(sourcePath))
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+                {
+                    Console.WriteLine(str);
+                }
+            }
+
+
+        }
+        catch { }
+
+    }
+}
+//Время запуска: 22.10.2022 21:15:30
---
+++ SF Module 8/8.4.1/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string strPath = @"C:\Users\Dima\Desktop\BinaryFile.bin";
+
+
+        BinaryTest.ReadFile(strPath);
+
+    }
+}
+
+
+
+class BinaryTest
+{
+    public static void ReadFile(string path)
+    {
+        FileInfo fileInfo = new FileInfo(path);
+
+        if (fileInfo.Exists)
+        {
+            try
+            {
+                using (BinaryReader binaryReader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    Console.WriteLine(binaryReader.ReadString());
+                }
+            }
+            catch { }
+        }
+    }
+}
