8498436a941bd17379cbb65b5bb253786f8a6dcc
Dmitry <ryabchikk@rambler.ru>
2022-10-22 22:02:25 +0300

    8.4.3

--- SF Module 8/8.4.1/Program.cs
+++ SF Module 8/8.4.1/Program.cs
@@ -3,8 +3,9 @@
     private static void Main(string[] args)
     {
         string strPath = @"C:\Users\Dima\Desktop\BinaryFile.bin";
+        string str = $"Файл изменен {DateTime.Now} на компьютере {Environment.OSVersion}";
 
-
+        BinaryTest.AddLastChange(strPath, str);
         BinaryTest.ReadFile(strPath);
 
     }
@@ -22,12 +23,30 @@ class BinaryTest
         {
             try
             {
-                using (BinaryReader binaryReader = new BinaryReader(File.Open(path, FileMode.Open)))
+                using (BinaryReader stream = new BinaryReader(File.Open(path, FileMode.Open)))
                 {
-                    Console.WriteLine(binaryReader.ReadString());
+                    Console.WriteLine(stream.ReadString());
                 }
             }
             catch { }
         }
     }
+
+    public static void AddLastChange(string path, string message)
+    {
+        FileInfo fileInfo = new FileInfo(path);
+
+        if (fileInfo.Exists)
+        {
+            try
+            {
+                using (BinaryWriter stream = new BinaryWriter(File.Open(path, FileMode.Open)))
+                {
+                    stream.Write(message);
+                }
+            }
+            catch { }
+        }
+    }
+
 }
---
+++ SF Module 8/8.4.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var person = new Contact("BLEAT", 841416341684684, "MEDVED@mail.ru");
+
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+
+        using(var fs = new FileStream("bufferSize.dat", FileMode.OpenOrCreate))
+        {
+            binaryFormatter.Serialize(fs, person);
+            Console.WriteLine("Объект сериализован");
+        }
+
+
+
+
+
+    }
+}
+
+[Serializable]
+class Contact
+{
+    public string Name { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string name, long phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+}
